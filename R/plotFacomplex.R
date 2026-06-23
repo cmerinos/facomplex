59d42c3f35d2b5e7fdd8626bece265b03647c9a2
Cesar Merino-Soto <sikayax@yahoo.com.ar>
2026-06-22 19:35:36 -0600

    more pre CRAN

--- R/plotFacomplex.R
+++ R/plotFacomplex.R
@@ -17,6 +17,12 @@
 #' @return A horizontal ggplot2 bar plot of item-level values.
 #'
 #' @examples
+#' # Data frame of factor loadings: 3 factors, 12 items; ESEM solution from published article
+#' ex1_fl <- data.frame(
+#' F1 = c(0.536, 0.708, 0.600, 0.673, 0.767, 0.481, -0.177, 0.209, -0.097, -0.115, 0.047, 0.024),
+#' F2 = c(-0.110, 0.026, 0.076, 0.011, -0.160, 0.106, 0.668, 0.438, 0.809, 0.167, 0.128, 0.041),
+#' F3 = c(-0.100, 0.036, 0.086, 0.021, -0.150, 0.116, 0.678, 0.448, 0.819, 0.577, 0.738, 0.751))
+#' 
 #' # Example using FSI output:
 #' FSIout <- simload(ex1_fl,  
 #'               items_target = list(F1 = c(1, 2, 3, 4, 5, 6),
